9d48b5c4aa77e60ecce171569f5a91ca1a44f012
Sean MacPherson <smacpherson@myst.ai>
2020-12-30 21:03:18 -0800

    404 needs fixing.

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+import "./404.css"
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <div className="not-found">
+        <h3>404 Not Found.</h3>
+      </div>
+    </Layout>
+  )
+}
